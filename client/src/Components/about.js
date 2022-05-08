7c4c900ccc81b61aad71e1313540eee81f6f35c3
Hamda Adan <Hamdamadan@gmail.com>
2022-05-08 21:09:19 +0100

    last change

--- client/src/Components/about.js
+++ client/src/Components/about.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
-const About=() => {
-    return (
-     <div> "About us!"</div>
-   )
-}
+const About = (props) => {
+  return (
+    <div className="header">
+      <h1>About</h1>
+     
+      <div className="page"> infomration about app  </div>
+    </div>
+  );
+};
+
 export default About;
